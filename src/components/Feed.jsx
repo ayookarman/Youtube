ab909c83cfcba5d5e80386be4e37954d4ce10f14
Karmanpreet Singh <karman.ind@gmail.com>
2024-10-09 19:26:43 +0530

    second

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -31,7 +31,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2023 by Anit Jha
+          Youtube Clone By Karmanpreet & Bakhshish
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
